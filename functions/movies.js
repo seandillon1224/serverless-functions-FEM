e0705c958bd1a7b49a81e96b28401e1d7accb7c5
Sean Dillon <seandillon1224@gmail.com>
2020-11-26 11:10:06 -0500

    Hook up and load movies from hasura

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,42 @@
-const movies = require('../data/movies.json');
+const {URL} = require('url');
+const fetch = require('node-fetch');
+// const movies = require('../data/movies.json');
+const { query } = require('./util/hasura');
 
 exports.handler = async () => {
+    const {movies} = await query({
+        query: `
+        query movies {
+            movies {
+              id
+              tagline
+              title
+              poster
+            }
+          }
+        `,
+    })
+    const api = new URL("https://www.omdbapi.com/");
+    // add secret api key
+    api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+
+    const promises = movies.map(async (movie) => {
+        // use movies imdb id to look up details
+        api.searchParams.set('i', movie.id);
+        const res = await fetch(api);
+        const movieData = await res.json();
+        const scores = movieData.Ratings;
+
+        return {
+            ...movie,
+            scores
+        }
+    });
+
+    const moviesWithRatings = await Promise.all(promises);
+
     return {
         statusCode: 200,
-        body: JSON.stringify(movies)
+        body: JSON.stringify(moviesWithRatings)
     }
 };
